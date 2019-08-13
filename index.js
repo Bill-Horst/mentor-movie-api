b56d4abb9a75e666c160969938204fbda2ef5939
Bill Horst <bill@billhorst.com>
2019-08-12 19:17:50 -0600

    changed db to actual movie db instead of test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const Users = Models.User;
 // mongoose.connect('mongodb://localhost:27017/movieApiDB', { useNewUrlParser: true });
 
 // deployed Mongo Atlas DB:
-mongoose.connect('mongodb+srv://movieApiDBAdmin:18j197ft5sf7@movieapidb-5sm08.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://movieApiDBAdmin:18j197ft5sf7@movieapidb-5sm08.mongodb.net/movieApiDB?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 app.use(bodyParser.json());
 app.use(morgan('common'));
