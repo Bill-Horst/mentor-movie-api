bf63e0d9de02bad2f5a0b05cbea1a2519de87ff7
Bill Horst <bill@billhorst.com>
2019-08-13 20:52:50 -0600

    removed CORS restrictions

--- index.js
+++ index.js
@@ -24,18 +24,7 @@ app.use(bodyParser.json());
 app.use(morgan('common'));
 app.use(validator());
 
-var allowedOrigins = ['*'];
-
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 app.use(express.static('public'));
 
